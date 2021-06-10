00b116fcd291db9de2610ac095bbc9c37d4e4b81
Alexander1990 <alexivanov2890@mail.ru>
2021-06-11 00:10:05 +0300

    added LevelScreen_14 and apgraded LevelScreen_8

--- core/src/alex/iv/rect/destroy/LevelScreen_14.java
+++ core/src/alex/iv/rect/destroy/LevelScreen_14.java
@@ -2,14 +2,25 @@ package alex.iv.rect.destroy;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 
 import alex.iv.rect.destroy.actors.Brick;
+import alex.iv.rect.destroy.actors.BrickHard;
 import alex.iv.rect.destroy.controller.BaseActor;
 import alex.iv.rect.destroy.controller.IActivityRequestHandler;
 import alex.iv.rect.destroy.controller.LevelScreenMain;
 import alex.iv.rect.destroy.controller.RectangleGame;
+import alex.iv.rect.destroy.controller.Warp;
 
 public class LevelScreen_14 extends LevelScreenMain {
+
+    private Warp warpMain1;
+    private Warp warpMain2;
+    private Warp warpMain3;
+    private Warp warp1;
+    private Warp warp2;
+    private Warp warp3;
+    private Warp warp4;
 
     // чтобы реклама появлялась, обязательно нужно использовать этод конструктор(с параметром requestHandler), в классе
     // MenuScreen
@@ -26,7 +37,7 @@ public class LevelScreen_14 extends LevelScreenMain {
 
         Brick tempBrick = new Brick(0,0,mainStage);
         tempBrick.remove();
-        int totalRows = 10;
+        int totalRows = 25;
         int totalCols = 10;
         float marginX = (Gdx.graphics.getWidth() - totalCols * tempBrick.getBrickWidth()) / 2;
         float marginY = (Gdx.graphics.getHeight() - totalRows * tempBrick.getBrickHeight()) - 50;
@@ -35,14 +46,262 @@ public class LevelScreen_14 extends LevelScreenMain {
                 float x = marginX + tempBrick.getBrickWidth()	* colNum;
                 float y = marginY + tempBrick.getBrickHeight() * rowNum;
                 Brick brick = new Brick( x, y, mainStage );
-                if (rowNum == 5 || rowNum == 6){
+                if (rowNum == 0 || rowNum == 1) {
+                    brick.setColor(Color.RED);
+                }
+                if (rowNum == 2 && colNum == 0 || rowNum == 2 && colNum == 5) {
+                    brick.setColor(Color.RED);
+                }
+                if (rowNum == 2 && colNum == 1 || rowNum == 2 && colNum == 6) {
                     brick.setColor(Color.ORANGE);
                     brick.numberColor = 2;
-                } else  if (rowNum == 7){
+                }
+                if (rowNum == 2 && colNum == 2 || rowNum == 2 && colNum == 7) {
                     brick.setColor(Color.YELLOW);
                     brick.numberColor = 3;
-                } else {
+                }
+                if (rowNum == 2 && colNum == 3) {
+                    brick.setColor(Color.GREEN);
+                    brick.numberColor = 4;
+                }
+                if (rowNum == 2 && colNum == 4) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                if (rowNum == 2 && colNum == 8) {
+                    brick.remove();
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                if (rowNum == 2 && colNum == 9) {
+                    brick.remove();
+                    warpMain1 = new Warp(x, y, "black_warp.png", mainStage);
+                }
+                /////////////////
+                if (rowNum == 3 && colNum == 0 || rowNum == 3 && colNum == 5) {
                     brick.setColor(Color.RED);
+                }
+                if (rowNum == 3 && colNum == 1 || rowNum == 3 && colNum == 6) {
+                    brick.setColor(Color.ORANGE);
+                    brick.numberColor = 2;
+                }
+                if (rowNum == 3 && colNum == 2 || rowNum == 3 && colNum == 7) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                }
+                if (rowNum == 3 && colNum == 3) {
+                    brick.setColor(Color.GREEN);
+                    brick.numberColor = 4;
+                }
+                if (rowNum == 3 && colNum == 4) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                if (rowNum == 3 && colNum == 8) {
+                    brick.remove();
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                if (rowNum == 3 && colNum == 9) {
+                    brick.remove();
+                }
+                //////////////////////////////////////
+                if (rowNum == 4 && colNum == 0 || rowNum == 4 && colNum == 5) {
+                    brick.setColor(Color.RED);
+                }
+                if (rowNum == 4 && colNum == 1 || rowNum == 4 && colNum == 6) {
+                    brick.setColor(Color.ORANGE);
+                    brick.numberColor = 2;
+                }
+                if (rowNum == 4 && colNum == 2 || rowNum == 4 && colNum == 7) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                }
+                if (rowNum == 4 && colNum == 3) {
+                    brick.setColor(Color.GREEN);
+                    brick.numberColor = 4;
+                }
+                if (rowNum == 4 && colNum == 4) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                if (rowNum == 4 && colNum == 8 || rowNum == 4 && colNum == 9) {
+                    brick.remove();
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                //////////////////////////////////////
+                if (rowNum == 5 && colNum == 0 || rowNum == 5 && colNum == 5) {
+                    brick.setColor(Color.RED);
+                }
+                if (rowNum == 5 && colNum == 1 || rowNum == 5 && colNum == 6) {
+                    brick.setColor(Color.ORANGE);
+                    brick.numberColor = 2;
+                }
+                if (rowNum == 5 && colNum == 2 || rowNum == 5 && colNum == 7) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                }
+                if (rowNum == 5 && colNum == 3 || rowNum == 5 && colNum == 8) {
+                    brick.setColor(Color.GREEN);
+                    brick.numberColor = 4;
+                }
+                if (rowNum == 5 && colNum == 4 || rowNum == 5 && colNum == 9) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                if (rowNum == 5 && colNum == 9) {
+                    warp1 = new Warp( x, y, mainStage);
+                }
+                //////////////////////////////////////
+                if (rowNum == 6 && colNum == 0 || rowNum == 6 && colNum == 9) {
+                    brick.setColor(Color.RED);
+                }
+                if (rowNum == 6 && colNum == 1 || rowNum == 6 && colNum == 8) {
+                    brick.setColor(Color.ORANGE);
+                    brick.numberColor = 2;
+                }
+                if (rowNum == 6 && colNum == 2 || rowNum == 6 && colNum == 7) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                }
+                if (rowNum == 6 && colNum == 3 || rowNum == 6 && colNum == 6) {
+                    brick.setColor(Color.GREEN);
+                    brick.numberColor = 4;
+                }
+                if (rowNum == 6 && colNum == 4 || rowNum == 6 && colNum == 5) {
+                    brick.setColor(Color.BLUE);
+                    brick.setBrickFlash(false);
+                }
+                if (rowNum == 6 && colNum == 2) {
+                    warp2 = new Warp( x, y, mainStage);
+                }
+                /////////////////////////////////////
+                if (rowNum == 7) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                }
+                /////////////////////////////////
+                if (rowNum == 8) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                    brick.mBrick = true;
+                }
+                //////////////////////////////////////
+                if (rowNum == 9) {
+                    brick.setColor(Color.GREEN);
+                    brick.numberColor = 4;
+                }
+                ///////////////////////////////////
+                if (rowNum == 10 || rowNum == 11 || rowNum == 12) {
+                    brick.setColor(Color.GREEN);
+                    brick.setBrickFlash(false);
+                }
+                if (rowNum == 10 && colNum == 5) {
+                    brick.remove();
+                    warpMain2 = new Warp(x, y, "black_warp.png", mainStage);
+                }
+                if (rowNum == 11 && colNum == 5) {
+                    brick.remove();
+                }
+                /////////////////////////////////////
+                if (rowNum == 13) {
+                    brick.setColor(Color.GREEN);
+                    brick.numberColor = 4;
+                }
+                if (rowNum == 13 && colNum == 1) {
+                    warp3 = new Warp( x, y, mainStage);
+                }
+                ///////////////////////////////////////
+                if (rowNum == 14) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                    brick.mBrick = true;
+                }
+                /////////////////////////////
+                if (rowNum == 15) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                }
+                if (rowNum == 16 && colNum == 0 || rowNum == 16 && colNum == 3 || rowNum == 16 && colNum == 6 || rowNum == 16 && colNum == 9) {
+                    brick.remove();
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                if (rowNum == 16 && colNum == 1 || rowNum == 16 && colNum == 2 || rowNum == 16 && colNum == 4 || rowNum == 16 && colNum == 5 || rowNum == 16 && colNum == 7 || rowNum == 16 && colNum == 8) {
+                    brick.setColor(Color.RED);
+                }
+                if (rowNum == 16 && colNum == 5) {
+                    warp4 = new Warp( x, y, mainStage);
+                }
+                //////////////////////////////////
+                if (rowNum == 17 || rowNum == 18) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                ///////////////////////////
+                if (rowNum == 19 && colNum == 0 || rowNum == 19 && colNum == 5) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                }
+                if (rowNum == 19 && colNum == 1 || rowNum == 19 && colNum == 6) {
+                    brick.setColor(Color.RED);
+                }
+                if (rowNum == 19 && colNum == 2 || rowNum == 19 && colNum == 7) {
+                    brick.setColor(Color.GREEN);
+                    brick.numberColor = 4;
+                }
+                if (rowNum == 19 && colNum == 3 || rowNum == 19 && colNum == 8) {
+                    brick.setColor(Color.ORANGE);
+                    brick.numberColor = 2;
+                }
+                if (rowNum == 19 && colNum == 4 || rowNum == 19 && colNum == 9) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                ////////////////////////////////////////////
+                if (rowNum == 20 && colNum == 2 || rowNum == 20 && colNum == 7) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                }
+                if (rowNum == 20 && colNum == 3 || rowNum == 20 && colNum == 8) {
+                    brick.setColor(Color.RED);
+                }
+                if (rowNum == 20 && colNum == 4 || rowNum == 20 && colNum == 9) {
+                    brick.setColor(Color.GREEN);
+                    brick.numberColor = 4;
+                }
+                if (rowNum == 20 && colNum == 0 || rowNum == 20 && colNum == 5) {
+                    brick.setColor(Color.ORANGE);
+                    brick.numberColor = 2;
+                }
+                if (rowNum == 20 && colNum == 1 || rowNum == 20 && colNum == 6) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                ////////////////////////////////////////////
+                if (rowNum == 21 && colNum == 4 || rowNum == 21 && colNum == 9) {
+                    brick.setColor(Color.YELLOW);
+                    brick.numberColor = 3;
+                }
+                if (rowNum == 21 && colNum == 0 || rowNum == 21 && colNum == 5) {
+                    brick.setColor(Color.RED);
+                }
+                if (rowNum == 21 && colNum == 1 || rowNum == 21 && colNum == 6) {
+                    brick.setColor(Color.GREEN);
+                    brick.numberColor = 4;
+                }
+                if (rowNum == 21 && colNum == 2 || rowNum == 21 && colNum == 7) {
+                    brick.setColor(Color.ORANGE);
+                    brick.numberColor = 2;
+                }
+                if (rowNum == 21 && colNum == 3 || rowNum == 21 && colNum == 8) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                if (rowNum == 21 && colNum == 0) {
+                    brick.remove();
+                    warpMain3 = new Warp(x, y, "black_warp.png", mainStage);
+                }
+                ////////////////////////////////////////////
+                if (rowNum == 22 || rowNum == 23 || rowNum == 24) {
+                    brick.remove();
                 }
             }
         }
@@ -57,6 +316,29 @@ public class LevelScreen_14 extends LevelScreenMain {
         }
         if (BaseActor.count(mainStage, "alex.iv.rect.destroy.actors.Brick") == 0 && starTimer > 0) {
             allTheBricksAreBroken(recordsLevel_14, "records_14");// инициализируем метод allTheBricksAreBroken - ВСЕ КИРПИЧИ РАЗРУШЕНЫ
+        }
+
+        for (BaseActor brick : BaseActor.getList(mainStage, "alex.iv.rect.destroy.actors.Brick")) {
+            if (Color.rgb888(brick.getColor()) == Color.rgb888(Color.ORANGE) && brick.mBrick ||
+                    Color.rgb888(brick.getColor()) == Color.rgb888(Color.RED) && brick.mBrick) {
+                brick.leftRightMoving(0, 2, 800, 900, 0, 180);
+            }
+        }
+
+        for (BaseActor bal : BaseActor.getList(mainStage, "alex.iv.rect.destroy.actors.Ball")) {
+            if (bal.overlaps(warpMain1) || bal.overlaps(warpMain2) || bal.overlaps(warpMain3)) {
+                int r = MathUtils.random(1, 4);
+                if (r == 1) {
+                    bal.centerAtActor(warp1);
+                } else if (r == 2){
+                    bal.centerAtActor(warp2);
+                } else if (r == 3) {
+                    bal.centerAtActor(warp3);
+                } else if (r == 4) {
+                    bal.centerAtActor(warp4);
+                }
+                bal.setMotionAngle(MathUtils.random(360));
+            }
         }
 
     }
--- core/src/alex/iv/rect/destroy/LevelScreen_8.java
+++ core/src/alex/iv/rect/destroy/LevelScreen_8.java
@@ -9,8 +9,15 @@ import alex.iv.rect.destroy.controller.BaseActor;
 import alex.iv.rect.destroy.controller.IActivityRequestHandler;
 import alex.iv.rect.destroy.controller.LevelScreenMain;
 import alex.iv.rect.destroy.controller.RectangleGame;
+import alex.iv.rect.destroy.controller.Warp;
 
 public class LevelScreen_8 extends LevelScreenMain {
+
+    private Warp warpMain1;
+    private Warp warp1;
+    private Warp warp2;
+    private Warp warp3;
+    private Warp warp4;
 
     public LevelScreen_8(IActivityRequestHandler requestHandler) {
         super(requestHandler);
@@ -25,20 +32,136 @@ public class LevelScreen_8 extends LevelScreenMain {
 
         Brick tempBrick = new Brick(0,0,mainStage);
         tempBrick.remove();
-        int totalRows = 10;
+        int totalRows = 24;
         int totalCols = 10;
         float marginX = (Gdx.graphics.getWidth() - totalCols * tempBrick.getBrickWidth()) / 2;
-        float marginY = (Gdx.graphics.getHeight() - totalRows * tempBrick.getBrickHeight()) - 150;
+        float marginY = (Gdx.graphics.getHeight() - totalRows * tempBrick.getBrickHeight()) - 50;
         for (int rowNum = 0; rowNum < totalRows; rowNum++) {
             for (int colNum = 0; colNum < totalCols; colNum++) {
                 float x = marginX + tempBrick.getBrickWidth()	* colNum;
                 float y = marginY + tempBrick.getBrickHeight() * rowNum;
                 Brick brick = new Brick( x, y, mainStage );
-                if (rowNum == 0 && colNum == 0 || rowNum == 0 && colNum == 2 || rowNum == 0 && colNum == 4 || rowNum == 0 && colNum == 6 || rowNum == 0 && colNum == 8){
+                if (rowNum == 0 || rowNum == 1) {
+                    brick.remove();
+                }
+                if (rowNum == 0 && colNum == 0) {
+                    warp1 = new Warp( x, y, mainStage);
+                }
+                if (rowNum == 0 && colNum == 9) {
+                    warp2 = new Warp( x, y, mainStage);
+                }
+                /////////////////////////////////
+                if (rowNum == 2 && colNum == 0) {
+                    brick.remove();
+                }
+                if (rowNum == 2 && colNum == 1 || rowNum == 2 && colNum == 3 || rowNum == 2 && colNum == 5 || rowNum == 2 && colNum == 7 || rowNum == 2 && colNum == 9) {
                     brick.remove();
                     BrickHard brickHard = new BrickHard(x, y, mainStage);
-                } else {
+                }
+                if (rowNum == 2 && colNum == 2 || rowNum == 2 && colNum == 4 || rowNum == 2 && colNum == 6 || rowNum == 2 && colNum == 8) {
+                    brick.setColor(Color.GREEN);
+                    brick.setBrickFlash(false);
+                }
+                //////////////////////////////////
+                if (rowNum == 3 && colNum == 0 || rowNum == 3 && colNum == 8) {
+                    brick.remove();
+                }
+                if (rowNum == 3 && colNum == 1 || rowNum == 3 && colNum == 2 || rowNum == 3 && colNum == 4 || rowNum == 3 && colNum == 7 || rowNum == 3 && colNum == 6) {
+                    brick.remove();
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                if (rowNum == 3 && colNum == 3 || rowNum == 3 && colNum == 5) {
+                    brick.setColor(Color.GREEN);
+                    brick.setBrickFlash(false);
+                }
+                if (rowNum == 3 && colNum == 9) {
+                    brick.remove();
+                    warpMain1 = new Warp(x, y, "black_warp.png", mainStage);
+                }
+                //////////////////////////////////
+                if (rowNum == 4 && colNum == 0 || rowNum == 4 && colNum == 8 || rowNum == 4 && colNum == 9 || rowNum == 4 && colNum == 3 || rowNum == 4 && colNum == 5) {
+                    brick.remove();
+                }
+                if (rowNum == 4 && colNum == 1 || rowNum == 4 && colNum == 7) {
+                    brick.remove();
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                if (rowNum == 4 && colNum == 2 || rowNum == 4 && colNum == 4 || rowNum == 4 && colNum == 6) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                ////////////////////////////////
+                if (rowNum == 5 && colNum == 0 || rowNum == 5 && colNum == 2 || rowNum == 5 && colNum == 4 || rowNum == 5 && colNum == 6) {
+                    brick.remove();
+                }
+                if (rowNum == 5 && colNum == 1 || rowNum == 5 && colNum == 7 || rowNum == 5 && colNum == 8 || rowNum == 5 && colNum == 9) {
+                    brick.remove();
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                if (rowNum == 5 && colNum == 3 || rowNum == 5 && colNum == 5) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                ///////////////////////////////////
+                if (rowNum == 6 && colNum == 0 || rowNum == 6 && colNum == 3 || rowNum == 6 && colNum == 5 || rowNum == 6 && colNum == 7 || rowNum == 6 && colNum == 9) {
+                    brick.remove();
+                }
+                if (rowNum == 6 && colNum == 1) {
+                    brick.remove();
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                if (rowNum == 6 && colNum == 2 || rowNum == 6 && colNum == 4 || rowNum == 6 && colNum == 6 || rowNum == 6 && colNum == 8) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                //////////////////////////////////////////
+                if (rowNum == 7 && colNum == 0 || rowNum == 7 && colNum == 2 || rowNum == 7 && colNum == 4 || rowNum == 7 && colNum == 6 || rowNum == 7 && colNum == 8) {
+                    brick.remove();
+                }
+                if (rowNum == 7 && colNum == 1) {
+                    brick.remove();
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                if (rowNum == 7 && colNum == 3 || rowNum == 7 && colNum == 5 || rowNum == 7 && colNum == 7 || rowNum == 7 && colNum == 9) {
+                    brick.setColor(Color.BLUE);
+                    brick.numberColor = 5;
+                }
+                /////////////////////////////////
+                if (rowNum == 8 || rowNum == 9 || rowNum == 10) {
+                    brick.remove();
+                }
+                if (rowNum == 8 && colNum == 1 || rowNum == 9 && colNum == 1) {
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                if (rowNum == 8 && colNum == 2) {
+                    warp3 = new Warp( x, y, mainStage);
+                }
+                if (rowNum == 10 && colNum != 0) {
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
+                }
+                /////////////////////////////////////
+                if (rowNum == 11 && colNum == 0 || rowNum == 11 && colNum == 1 || rowNum == 11 && colNum == 2 || rowNum == 11 && colNum == 3 || rowNum == 11 && colNum == 5 || rowNum == 11 && colNum == 6 || rowNum == 11 && colNum == 7 || rowNum == 11 && colNum == 8) {
+                    brick.remove();
+                }
+                if (rowNum == 11 && colNum == 4 || rowNum == 11 && colNum == 9) {
                     brick.setColor(Color.RED);
+                }
+                /////////////////////////////////////
+                if (rowNum == 12 && colNum == 0 || rowNum == 12 && colNum == 2 || rowNum == 12 && colNum == 3 || rowNum == 12 && colNum == 4 || rowNum == 12 && colNum == 5 || rowNum == 12 && colNum == 6 || rowNum == 12 && colNum == 7 || rowNum == 12 && colNum == 8) {
+                    brick.remove();
+                }
+                if (rowNum == 12 && colNum == 1 || rowNum == 12 && colNum == 9) {
+                    brick.setColor(Color.RED);
+                }
+                //////////////////////////////////////
+                if (rowNum == 14 && colNum != 9 || rowNum == 13) {
+                    brick.remove();
+                }
+                if (rowNum == 14 && colNum == 9) {
+                    brick.setColor(Color.RED);
+                }
+                if (rowNum == 14 && colNum == 0 || rowNum == 14 && colNum == 1 || rowNum == 14 && colNum == 2 || rowNum == 14 && colNum == 3 || rowNum == 14 && colNum == 4 || rowNum == 14 && colNum == 5 || rowNum == 14 && colNum == 6 || rowNum == 14 && colNum == 7) {
+                    BrickHard brickHard = new BrickHard(x, y, mainStage);
                 }
             }
         }
